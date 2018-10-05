9a82956845ccc3cae367aa851e68be5446e1deee
Данила <guseffdanila@ya.ru>
2018-10-05 15:01:23 +0300

    clean main function

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
 from task1.solution1 import solution1
-from task2.solution2 import solution2, find_all_tokens
+from task2.solution2 import solution2
 
 
 def main():
-    #print(find_all_tokens(['mission—this']))
     solution1('anaconda.txt', 'result1.txt')
     solution2('anaconda.txt', 'result2.1.txt', 'result2.2.txt')
 
